c8fc12d3d8ce4515f287b9b1264f386cc1240fd3
Surbma <peter@surbma.hu>
2015-04-02 12:25:47 +0200

    1.4.1
    - Fix localization.
    - Prevent direct access to the plugin.

--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: http://surbma.com/
 Tags: uikit, buttons, font awesome, icon, tab, accordion, modal
 Requires at least: 3.1
 Tested up to: 4.1
-Stable tag: 1.4.0
+Stable tag: 1.4.1
 License: GPLv2
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -57,6 +57,11 @@ Nope. This is only a selection of the most used styles from UIkit.
 It is the reverse version of my last name. ;)
 
 == Changelog ==
+
+= 1.4.1 =
+
+- Fix localization.
+- Prevent direct access to the plugin.
 
 = 1.4.0 =
 
--- surbma-uikit-2-almost-flat.php
+++ surbma-uikit-2-almost-flat.php
@@ -5,7 +5,7 @@ Plugin Name: Surbma - UIkit 2 - Almost Flat
 Plugin URI: http://surbma.com/wordpress-plugins/
 Description: Some useful UIkit 2 styles for your site.
 
-Version: 1.4.0
+Version: 1.4.1
 
 Author: Surbma
 Author URI: http://surbma.com/
@@ -16,11 +16,16 @@ Text Domain: surbma-uikit-2-almost-flat
 Domain Path: /languages/
 */
 
+// Prevent direct access to the plugin
+if ( !defined( 'ABSPATH' ) ) {
+	die( 'Good try! :)' );
+}
+
 // Localization
 function surbma_uikit_2_almost_flat_init() {
-	load_plugin_textdomain( 'surbma-uikit-2-almost-flat', false, dirname( plugin_basename( __FILE__ ) . '/languages/' ) );
+	load_plugin_textdomain( 'surbma-uikit-2-almost-flat', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 }
-add_action( 'init', 'surbma_uikit_2_almost_flat_init' );
+add_action( 'plugins_loaded', 'surbma_uikit_2_almost_flat_init' );
 
 function surbma_uikit_2_almost_flat_enqueue_scripts() {
 	wp_enqueue_script( 'uikit-scripts', plugins_url( '', __FILE__ ) . '/js/uikit-min.js', array( 'jquery' ), '2.12.0', true );
